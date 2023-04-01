3413d6edaa1e32858f44146defbadb4f484f3df7
Alessandro Betti <alessandrobetti53@yahoo.it>
2023-04-01 16:11:40 +0200

    Testing CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,11 +4,12 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-
-
 app = FastAPI(title="Predicting Wine Class with batching")
 
 # Open classifier in global scope
+# Remark: the classifier is loaded directly into global state instead of within a 
+# function that runs when the server is started. This is done because you will be performing unit 
+# tests on the classifier without starting the server
 with open("models/wine.pkl", "rb") as file:
     clf = pickle.load(file)
 
